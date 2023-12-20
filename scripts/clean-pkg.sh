50073efd385f6a0e7946d8665f7149c173affa41
David Fernández Calle <david.fernandez@nextail.co>
2023-12-20 11:29:28 +0100

    update scripts

--- scripts/clean-pkg.sh
+++ scripts/clean-pkg.sh
@@ -3,3 +3,4 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 find ${SCRIPTPATH}/.. -name '__pypackages__' -exec rm -fr {} +
 find ${SCRIPTPATH}/.. -name '.pdm.toml' -exec rm -fr {} +
 find ${SCRIPTPATH}/.. -name '.venv' -exec rm -fr {} +
+find ${SCRIPTPATH}/.. -name '.pdm-build' -exec rm -fr {} +
