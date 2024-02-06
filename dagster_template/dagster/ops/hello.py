fe07e306f02cf9f26723bbcc81e41e0c04d54c15
David Fernández Calle <david.fernandez@nextail.co>
2024-02-06 16:52:24 +0100

    feature: update versions

--- dagster_template/dagster/ops/hello.py
+++ dagster_template/dagster/ops/hello.py
@@ -1,11 +1,13 @@
 from dagster import op
 
 
-@op(description="""
+@op(
+    description="""
     An op definition. This example op outputs a single string.
 
     For more hints about writing Dagster ops, see our documentation overview on Ops:
     https://docs.dagster.io/concepts/ops-jobs-graphs/ops
-    """)
+    """
+)
 def hello():
     return "Hello, Dagster!"
