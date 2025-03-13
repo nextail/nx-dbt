aaffb4f5ab8a366accdc4c1a064b248a71245038
Pablo Espeso <pablo.espeso@nextail.co>
2025-03-13 15:21:56 +0100

    set new keys for dbt query tag

--- nx_dbt/dbt_project/macros/query_tags.sql
+++ nx_dbt/dbt_project/macros/query_tags.sql
@@ -16,30 +16,13 @@
                 'model_database': model.database,
                 'model_schema': model.schema,
                 'model_name': model.name,
-                'model_resource_type': model.resource_type,
-                'model_package_name': model.package_name,
-                'model_path': model.path,
                 'model_original_file_path': model.original_file_path,
-                'model_unique_id': model.unique_id,
-                'model_fqn': model.fqn,
                 'model_alias': model.alias,
-                'model_checksum': model.checksum,
                 'model_config': model.config,
                 
                 'model_tags': model.tags or None,
-                'model_description': model.description or None,
-                'model_columns': model.columns or None,
-                'model_meta': model.meta or None,
-                'model_group': model.group or None,
-                
-                
-                
-                'model_docs': model.docs or None,
-                'model_patch_path': model.patch_path or None,
-                'model_build_path': model.build_path or None,
 
                 'model_created_at': model.created_at or None,
-                'model_root_path': model.root_path or None
             }            
         }
     )) %}
