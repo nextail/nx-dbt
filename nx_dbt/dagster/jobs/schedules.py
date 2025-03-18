ac78cc0bcd36a39d08b609e9ea5bbae35543a558
Pablo Espeso <pablo.espeso@nextail.co>
2025-03-18 12:10:10 +0100

    working scaffolding

--- nx_dbt/dagster/jobs/schedules.py
+++ nx_dbt/dagster/jobs/schedules.py
@@ -9,15 +9,16 @@ schedules = [
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
         job_name="materialize_all",
-        cron_schedule="0 */4 * * *", # every 4 hours starting at 00:00
+        cron_schedule="0 */1 * * *", # every 1 hour starting at 00:00
         # dbt_select="+context.selected_asset_keys+",
         dbt_select="fqn:*"
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
         job_name="materialize_snowflake",
-        cron_schedule="*/5 * * * *", # every 5 minutes
+        cron_schedule="*/30 * * * *", # every 30 minutes starting at 00:00
         # dbt_select="+context.selected_asset_keys+",
-        dbt_select="fqn:+staging.snowflake+"
+        dbt_select="fqn:staging.snowflake.stg_query_attribution_history"
+        # dbt_select="source:snowflake+"
     ),
 ]
