5978db1f2a811402f9a0528b8f1a17e405499c1e
David Fernández <david.fernandez@nextail.co>
2025-03-26 18:06:33 +0100

    PLATFORM-2356 fix sensor tick delay (#45)
    * PLATFORM-2356 - new orb grpc workers configuration
    
    * PLATFORM-2360 - remove deprecated library
    
    * PLATFORM-2360 - upgrade libraries

--- dagster_template/dagster/__init__.py
+++ dagster_template/dagster/__init__.py
@@ -1,12 +1,8 @@
-import warnings
-
-from dagster import Definitions, ExperimentalWarning
+from dagster import Definitions
 
 from dagster_template.dagster.jobs.say_hello import say_hello_job
 from dagster_template.dagster.schedules.my_hourly_schedule import my_hourly_schedule
 from dagster_template.dagster.sensors.my_sensor import my_sensor
-
-warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
 defs = Definitions(
     jobs=[say_hello_job],
