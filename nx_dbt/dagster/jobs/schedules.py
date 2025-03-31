95c3553fffc7becff8484f9bd15db95a32ee8cb6
Pablo Espeso <pablo.espeso@nextail.co>
2025-03-31 18:31:05 +0200

    set new dagster schedules

--- nx_dbt/dagster/jobs/schedules.py
+++ nx_dbt/dagster/jobs/schedules.py
@@ -6,19 +6,28 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from .assets import nx_internal_reporting_full
 
 schedules = [
+    # docs https://docs.dagster.io/api/python-api/libraries/dagster-dbt#dagster_dbt.build_schedule_from_dbt_selection
+    # parameters:
+    #   job_name (str) – The name of the job to materialize the dbt resources.
+    #   cron_schedule (str) – The cron schedule to define the schedule.
+    #   dbt_select (str) – A dbt selection string to specify a set of dbt resources.
+    #   dbt_exclude (Optional[str]) – A dbt selection string to exclude a set of dbt resources.
+    #   schedule_name (Optional[str]) – The name of the dbt schedule to create.
+    #   tags (Optional[Mapping[str, str]]) – A dictionary of tags (string key-value pairs) to attach to the scheduled runs.
+    #   config (Optional[RunConfig]) – The config that parameterizes the execution of this schedule.
+    #   execution_timezone (Optional[str]) – Timezone in which the schedule should run. Supported strings for timezones are the ones provided by the IANA time zone database <https://www.iana.org/time-zones> - e.g. “America/Los_Angeles”.
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
-        job_name="materialize_all",
+        job_name="costs_materialization",
         cron_schedule="0 */1 * * *", # every 1 hour starting at 00:00
-        # dbt_select="+context.selected_asset_keys+",
-        dbt_select="fqn:*"
+        dbt_select="fqn:costs.*",
+        schedule_name="costs_materialization",
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
         job_name="materialize_snowflake",
         cron_schedule="*/30 * * * *", # every 30 minutes starting at 00:00
-        # dbt_select="+context.selected_asset_keys+",
-        dbt_select="fqn:staging.snowflake.stg_query_attribution_history"
-        # dbt_select="source:snowflake+"
+        dbt_select="fqn:staging.snowflake.stg_query_attribution_history+",
+        schedule_name="materialize_snowflake",
     ),
 ]
