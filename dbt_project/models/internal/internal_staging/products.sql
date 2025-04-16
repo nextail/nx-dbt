dec043416b7efbb9db8066e6bfafac309686b697
Pablo Espeso <pablo.espeso@nextail.co>
2025-04-16 17:54:23 +0200

    nx internal models

---
+++ dbt_project/models/internal/internal_staging/products.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = ['id', 'tenant'],
+        incremental_strategy = 'merge',
+        on_schema_change = 'sync_all_columns',
+        cluster_by = ['tenant']
+    )
+}}
+-- database names: aristocrazy_main_prod_db, suarez_main_prod_db
+
+{% for tenant in var('all_tenants') %}
+
+select
+    *,
+    '{{ tenant }}' as tenant,
+from {{ source(tenant + '_globaldomain_public', 'products') }}
+
+{% if is_incremental() %}
+    where updated_at > (select max(updated_at) from {{ this }}) and tenant = '{{ tenant }}'
+{% endif %}
+
+    {% if not loop.last and var('all_tenants') | length > 1 %}
+    -- will append the next tenant if there are more than one tenant
+    union all
+    {% endif %}
+
+{% endfor %} 
---
+++ dbt_project/models/internal/internal_staging/sales.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = ['store_id', 'sku_id', 'date', 'tenant'],
+        incremental_strategy = 'merge',
+        on_schema_change = 'sync_all_columns',
+        cluster_by = ['tenant'],
+        enabled = false
+    )
+}}
+-- database names: aristocrazy_main_prod_db, suarez_main_prod_db
+
+{% for tenant in var('all_tenants') %}
+
+select
+    *,
+    '{{ tenant }}' as tenant,
+from {{ source(tenant + '_globaldomain_public', 'sales') }}
+
+{% if is_incremental() %}
+    where date >= (select max(date) from {{ this }}) and tenant = '{{ tenant }}'
+{% endif %}
+
+    {% if not loop.last and var('all_tenants') | length > 1 %}
+    -- will append the next tenant if there are more than one tenant
+    union all
+    {% endif %}
+
+{% endfor %} 
