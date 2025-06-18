2f7d58b680eb338f6b07ca331198a0f23fe5b391
Pablo Espeso <pablo.espeso@nextail.co>
2025-06-18 17:43:27 +0200

    mock dagster tests

---
+++ tests/dagster/ops/test_dbt_debug.py
@@ -0,0 +1,13 @@
+from nx_dbt.dagster.ops.hello import hello
+
+
+def test_hello():
+    """
+    Test idea
+
+    This test should run "dbt debug" and assert that the output is successful.
+
+    To be completed.
+    """
+
+    assert True
