d28d9c00d60c717d5929177033194523c49d94ed
Pablo Espeso <pablo.espeso@nextail.co>
2025-06-18 12:30:54 +0200

    fixed dagster local loading error

--- nx_dbt/dagster/__init__.py
+++ nx_dbt/dagster/__init__.py
@@ -1,16 +1,10 @@
-import warnings
+# from dagster import Definitions
 
-from dagster import Definitions, ExperimentalWarning
-
-# from nx_dbt.dagster.jobs.say_hello import say_hello_job
-# from nx_dbt.dagster.schedules.my_hourly_schedule import my_hourly_schedule
-# from nx_dbt.dagster.sensors.my_sensor import my_sensor
-
-# from nx_dbt.dagster.jobs.say_hello import daily_dbt_assets_schedule, dbt_project
+# from dagster_template.dagster.jobs.say_hello import say_hello_job
+# from dagster_template.dagster.schedules.my_hourly_schedule import my_hourly_schedule
+# from dagster_template.dagster.sensors.my_sensor import my_sensor
 
 from nx_dbt.dagster.jobs.definitions import defs
-
-warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
 defs = defs
 
