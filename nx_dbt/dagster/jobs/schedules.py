40cdad3d8bff104eca2b148bd32b1c2cdca7b8a9
Pablo Espeso <pablo.espeso@nextail.co>
2025-06-20 15:29:21 +0200

    set scheduled to state running at start

--- nx_dbt/dagster/jobs/schedules.py
+++ nx_dbt/dagster/jobs/schedules.py
@@ -2,21 +2,12 @@
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
 from dagster_dbt import build_schedule_from_dbt_selection
-from dagster import DefaultSensorStatus
+from dagster import DefaultScheduleStatus
 
 from .assets import nx_internal_reporting_full
 
 schedules = [
     # docs https://docs.dagster.io/api/python-api/libraries/dagster-dbt#dagster_dbt.build_schedule_from_dbt_selection
-    # parameters:
-    #   job_name (str) – The name of the job to materialize the dbt resources.
-    #   cron_schedule (str) – The cron schedule to define the schedule.
-    #   dbt_select (str) – A dbt selection string to specify a set of dbt resources.
-    #   dbt_exclude (Optional[str]) – A dbt selection string to exclude a set of dbt resources.
-    #   schedule_name (Optional[str]) – The name of the dbt schedule to create.
-    #   tags (Optional[Mapping[str, str]]) – A dictionary of tags (string key-value pairs) to attach to the scheduled runs.
-    #   config (Optional[RunConfig]) – The config that parameterizes the execution of this schedule.
-    #   execution_timezone (Optional[str]) – Timezone in which the schedule should run. Supported strings for timezones are the ones provided by the IANA time zone database <https://www.iana.org/time-zones> - e.g. “America/Los_Angeles”.
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
         job_name="costs_materialization_job",
@@ -25,6 +16,7 @@ schedules = [
         execution_timezone="UTC",
         dbt_select="fqn:costs.*",
         schedule_name="costs_materialization_schedule",
+        default_status=DefaultScheduleStatus.RUNNING
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
@@ -34,13 +26,15 @@ schedules = [
         execution_timezone="UTC",
         dbt_select="fqn:internal.*",
         schedule_name="internal_materialization_schedule",
+        default_status=DefaultScheduleStatus.RUNNING
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
         job_name="snowflake_query_attribution_job",
-        cron_schedule="5 */4 * * *", # each four hours starting at 00:05
+        cron_schedule="5 */4 * * *", # each four hour starting at 00:05
         execution_timezone="UTC",
         dbt_select="fqn:stg_query_attribution_history",
         schedule_name="snowflake_query_attribution_schedule",
+        default_status=DefaultScheduleStatus.RUNNING
     ),
 ]
