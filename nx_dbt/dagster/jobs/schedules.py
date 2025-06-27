81a96638aaa474710a8b1c4d5d28ace5836476b2
Pablo Espeso <pablo.espeso@nextail.co>
2025-06-27 09:45:40 +0200

    update jobs and schedule names

--- nx_dbt/dagster/jobs/schedules.py
+++ nx_dbt/dagster/jobs/schedules.py
@@ -38,33 +38,33 @@ schedules = [
     # docs https://docs.dagster.io/api/python-api/libraries/dagster-dbt#dagster_dbt.build_schedule_from_dbt_selection
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
-        job_name="costs_materialization_job",
+        job_name="dbt_costs_materialization_job",
         cron_schedule="0 */12 * * *", # every 12 hours starting at 00:00
         # cron_schedule="10 * * * *", # every hour at minute 10
         execution_timezone="UTC",
         dbt_select="fqn:costs.*",
-        schedule_name="costs_materialization_schedule",
+        schedule_name="dbt_costs_materialization_schedule",
         default_status=DefaultScheduleStatus.RUNNING,
         tags=get_tags_with_operation("costs_materialization"),
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
-        job_name="internal_materialization_job",
+        job_name="dbt_internal_materialization_job",
         cron_schedule="0 5 * * *", # daily at 05:00 UTC
         # cron_schedule="*/10 * * * *", # every 10 minutes for testing
         execution_timezone="UTC",
         dbt_select="fqn:internal.*",
-        schedule_name="internal_materialization_schedule",
+        schedule_name="dbt_internal_materialization_schedule",
         default_status=DefaultScheduleStatus.RUNNING,
         tags=get_tags_with_operation("internal_materialization"),
     ),
     build_schedule_from_dbt_selection(
         [nx_internal_reporting_full],
-        job_name="snowflake_query_attribution_job",
+        job_name="dbt_snowflake_query_attribution_job",
         cron_schedule="5 */4 * * *", # each four hour starting at 00:05
         execution_timezone="UTC",
         dbt_select="fqn:stg_query_attribution_history",
-        schedule_name="snowflake_query_attribution_schedule",
+        schedule_name="dbt_snowflake_query_attribution_schedule",
         default_status=DefaultScheduleStatus.RUNNING,
         tags=get_tags_with_operation("snowflake_query_attribution"),
     ),
